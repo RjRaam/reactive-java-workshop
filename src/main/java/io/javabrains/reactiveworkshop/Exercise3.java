fbf878517db4caff3abbfb90cf8cec0780a918b0
Ramprakash <ram7prksh@yahoo.co.in>
2023-09-06 18:36:20 +0530

    Code implementation

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,20 +1,21 @@
 package io.javabrains.reactiveworkshop;
 
-import java.io.IOException;
+import java.util.List;
 
 public class Exercise3 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         // Use ReactiveSources.intNumbersFlux()
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
-        // TODO: Write code here
+        List<Integer> numList = ReactiveSources.intNumbersFlux().toStream().toList();
+        numList.forEach(System.out::println);
+        System.out.println(numList.size());
 
-
-        System.out.println("Press a key to end");
-        System.in.read();
+//        System.out.println("Press a key to end");
+//        System.in.read();
     }
 
 }
